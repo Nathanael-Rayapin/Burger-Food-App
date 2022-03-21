47ac2d2c5eb6498a1956f518630d0c9869f4c323
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-21 22:50:36 +0100

    navbar header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,26 +1,28 @@
-import { Fragment } from 'react';
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import classes from './Header.module.css';
 
 const Header = (props) => {
+    const location = useLocation();
+
     return (
-        <Fragment>
-            <div className={classes.navbar}>
-                <h1 className={classes.title}>
-                    <span className={classes['burger-span']}>Burger </span>
-                    <span className={classes['king-span']}>King</span>
-                </h1>
-                <ul className={classes.list}>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Feature</li>
-                    <li>Chef</li>
-                    <li>Menu</li>
-                    <li>Booking</li>
-                    <li>Page</li>
-                    <li>Contact</li>
-                </ul>
-            </div>
-        </Fragment>
+        <header className={`${classes.navbar} ${location.pathname === '/menu' && classes.notHome}`}>
+            <h1 className={classes.title}>
+                <span className={classes['burger-span']}>Burger </span>
+                <span className={classes['king-span']}>Coding</span>
+            </h1>
+            <ul className={classes.list}>
+                <li>
+                    <Link to='/home'>Home</Link>
+                </li>
+                <li>
+                    <Link to='/menu'>Menu</Link>
+                </li>
+                <li>
+                    <Link to='/booking'>Booking</Link>
+                </li>
+            </ul>
+        </header>
     );
 };
 
