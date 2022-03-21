ab9656907ee644661dda1f7d2706c372c07dfbec
Nathanael421 <nathanael.rayapin@gmail.com>
2022-03-21 22:50:01 +0100

    intro access

---
+++ src/components/Home/Introduction/Introduction-Access/Introduction-Access.js
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom';
+import classes from './Introduction-Access.module.css';
+
+const IntroductionAccess = (props) => {
+    return (
+        <div className={classes['intro-access']}>
+            <h1>World's <span>Best </span>Chef</h1>
+            <p>
+                Morbi sagittis turpis id suscipit feugiat. Suspendisse eu augue urna. Morbi<br /> sagittis,
+                orci sodales varius fermentum, tortor
+            </p>
+            <div>
+                <Link to='/menu'>
+                    <button className={classes['button-view']}>View Menu</button>
+                </Link>
+
+                <button className={classes['button-book']}>Book Table</button>
+            </div>
+        </div>
+    );
+};
+
+export default IntroductionAccess;
